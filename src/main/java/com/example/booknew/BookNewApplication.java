7f592a388eed16a2a81de155c68034d2f0fcc7e7
Anastasiyu <asy.safiulina@mail.ru>
2022-09-30 13:22:46 +0300

    Initial commit

---
+++ src/main/java/com/example/booknew/BookNewApplication.java
@@ -0,0 +1,13 @@
+package com.example.booknew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookNewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookNewApplication.class, args);
+    }
+
+}
